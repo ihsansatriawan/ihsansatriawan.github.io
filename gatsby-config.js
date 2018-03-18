b7c377f5a9f0ae2d24bb5708ad196e8de280d25a
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2018-03-19 06:56:15 +0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     author: "Aman Mittal",
     description: "A Gatsby.js Starter based on Ceevee by Styleshout"
   },
-  pathPrefix: "/ihsansatriawan.github.io",
+  pathPrefix: "/",
   plugins: [
     {
       resolve: `gatsby-plugin-typography`,
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,7 +15,7 @@ const Skills = () => (
         illo inventore veritatis et quasi architecto beatae vitae dicta sunt
         explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
         odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt.
+        voluptatem sequi nesciunt. xzjkhdkashdaks
       </p>
 
       <div className="bars">
